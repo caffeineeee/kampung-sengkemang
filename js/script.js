4a7911e6da662c8b7cd0af80de6d9296d05c1614
itscevinsam <cevinsam11@gmail.com>
2022-08-08 10:47:11 +0700

    init

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function whatsapp() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var phone = document.getElementById("phone").value;
+  var date = document.getElementById("date").value;
+  var message = document.getElementById("message").value;
+
+  var url =
+    "https://wa.me/6285162745705?text=" +
+    "*Name:* " +
+    name +
+    "%0a" +
+    "*Email:* " +
+    email +
+    "%0a" +
+    "*Phone:* " +
+    phone +
+    "%0a" +
+    "*Date:* " +
+    date +
+    "%0a" +
+    "*Message:* " +
+    message;
+
+  window.open(url, "_blank").focus();
+}
